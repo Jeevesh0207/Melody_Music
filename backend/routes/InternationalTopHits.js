6ca96f32cffea3b504ab2251e0989b4b9b6cc45d
Jeevesh0207 <r27abbit@gmail.com>
2023-12-19 13:43:38 +0530

    Add Song In TopHits,International,Romantic

--- backend/routes/InternationalTopHits.js
+++ backend/routes/InternationalTopHits.js
@@ -5,60 +5,84 @@ InternationalTopHits.get('/',(req,res)=>{
     const array=[
         {
             id:1,
+            Name:"Without Me",
+            Singer:"Halsey",
+            ID:"0H7txPklqAI"   
+        },
+        {
+            id:2,
+            Name:"Despacito",
+            Singer:"Luis Fonsi",
+            ID:"gm3-m2CFVWM"   
+        },
+        {
+            id:3,
+            Name:"Taki Taki",
+            Singer:"DJ Snake, Selena Gomez",
+            ID:"Ov3mSO5SVg8"   
+        },
+        {
+            id:4,
+            Name:"Waka Waka (This Time For Africa)",
+            Singer:"Shakira",
+            ID:"czWcyZRAMtk"   
+        },
+        {
+            id:5,
             Name:"Calm Down",
             Singer:"Rema, Selena Gomez",
             ID:"CQLsdm1ZYAw"   
         },
         {
-            id:2,
+            id:6,
             Name:"Unstoppable",
             Singer:"Sia",
             ID:"jJvDnYdD8JQ"   
         },
         {
-            id:3,
+            id:7,
             Name:"Gasolina",
             Singer:"Daddy Yankee",
             ID:"4CTBznuwV4w"   
         },
         {
-            id:4,
+            id:8,
             Name:"Perfect",
             Singer:"Ed Sheeran",
             ID:"2Vv-BfVoq4g"   
         },
         {
-            id:5,
+            id:9,
             Name:"Bones",
             Singer:"Imagine Dragons",
             ID:"ZNsPYmkSPeI"   
         },
         {
-            id:6,
+            id:10,
             Name:"Love Me Like You Do",
             Singer:"Ellie Goulding",
             ID:"bgjUzhdmmF0"   
         },
         {
-            id:7,
+            id:11,
             Name:"Let Me Love You",
             Singer:"DJ Snake",
             ID:"SMs0GnYze34"   
         },
         {
-            id:8,
+            id:12,
             Name:"Believer",
             Singer:"Imagine Dragons",
             ID:"W0DM5lcj6mw"   
         },
         {
-            id:9,
+            id:13,
             Name:"People",
             Singer:"Libianca",
             ID:"MBF1mTW4kFY"   
         },
         {
-            id:10,
+            id:14,
             Name:"Señorita",
             Singer:"Shawn Mendes, Camila Cabello",
             ID:"ow1QqW0jzTo"   
--- backend/routes/TopHits.js
+++ backend/routes/TopHits.js
@@ -5,66 +5,90 @@ TopHits.get('/',(req,res)=>{
     const array=[
         {
             id:1,
+            Name:"Moye Moye",
+            Singer:"Teya Dora",
+            ID:"jD97hNDiyvI" 
+        },
+        {
+            id:2,
+            Name:"Aziyat",
+            Singer:"Pratyush Dhiman",
+            ID:"5k58kWEuwJQ" 
+        },
+        {
+            id:3,
+            Name:"Tere Hawaale",
+            Singer:"Arijit Singh & Shilpa Rao",
+            ID:"KUpwupYj_tY" 
+        },
+        {
+            id:4,
+            Name:"Cheques",
+            Singer:"Shubh",
+            ID:"4tywp83zkmk" 
+        },
+        {
+            id:5,
             Name:"Pehle Bhi Main",
             Singer:"Vishal Mishra",
             ID:"QKMTreKTpug"   
         },
         {
-            id:2,
+            id:6,
             Name:"Jaan Denge Tumhe",
             Singer:"Laqshay Kapoor",
             ID:"D9fbtKjJ8rQ"   
         },
         {
-            id:3,
+            id:7,
             Name:"Yaad Ban Gaye",
             Singer:"Tulsi Kumar & Manan Bhardwaj",
             ID:"oaUwdKG2UQs"   
         },
         {
-            id:4,
+            id:8,
             Name:"Heeriye",
             Singer:"Arijit Singh & Jasleen Royal",
             ID:"RLzC55ai0eo"   
         },
         {
-            id:5,
+            id:9,
             Name:"Aisi Teri Yaadein",
             Singer:"Jubin Nautiyal",
             ID:"ly4moi2plX4"   
         },
         {
-            id:6,
+            id:10,
             Name:"Baby ft. Ludacris",
             Singer:"Justin Bieber",
             ID:"khOFw2f4bQY"   
         },
         {
-            id:7,
+            id:11,
             Name:"Suraj Hua Maddham",
             Singer:"Alka Yagnik, Sonu Nigam",
             ID:"D0V8S0elRyA"   
         },
         {
-            id:8,
+            id:12,
             Name:"Chaand Baaliyan",
             Singer:"Aditya A.",
             ID:"7c3-Gei5j4w"   
         },
         {
-            id:9,
+            id:13,
             Name:"Tera Zikr",
             Singer:"Darshan Raval",
             ID:"HscrSwilshM"   
         },
         {
-            id:10,
+            id:14,
             Name:"Kali Kali Zulfon Ke",
             Singer:"Jubin Nautiyal, Rochak Kohli",
             ID:"224EFEddxMk"   
         },
         {
-            id:11,
+            id:15,
             Name:"Satranga",
             Singer:"Arjit Singh",
             ID:"HrnrqYxYrbk"   
