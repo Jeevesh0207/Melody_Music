46d51313ba9e8c4c310eee40f78f99e195ecbd17
Jeevesh0207 <r27abbit@gmail.com>
2024-01-03 13:49:57 +0530

    Try 16

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -52,10 +52,10 @@ router.use('/', ProfileData);
 router.use('/', AddToFavourite);
 
 router.use('/sliderimages', express.static('../public/image/Slider'));
-// router.use('/singerplaylist', express.static('../public/image/Playlist'));
-// router.use('/uploads', express.static('../uploads'))
-// router.use('/tophitsimages', express.static('../public/image/TopHits'));
-// router.use('/internationaltophits', express.static('../public/image/InternationalTopHits'));
-// router.use('/romantic', express.static('../public/image/Romantic'));
+router.use('/singerplaylist', express.static('../public/image/Playlist'));
+router.use('/uploads', express.static('../uploads'))
+router.use('/tophitsimages', express.static('../public/image/TopHits'));
+router.use('/internationaltophits', express.static('../public/image/InternationalTopHits'));
+router.use('/romantic', express.static('../public/image/Romantic'));
 
 module.exports = router;
